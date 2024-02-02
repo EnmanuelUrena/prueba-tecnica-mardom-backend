c343f374d12dfeff5398cf91f1a0190d77fb49b6
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2024-02-01 22:34:56 -0400

    Get Empleados

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prueba_tecnica_mardom_01.Models;
+using System.Text.Json;
+
+
+namespace Prueba_tecnica_mardom_01.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmpleadoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetAll()
+        {
+            var textFilePath = Path.Combine(Directory.GetCurrentDirectory(), "data.txt");
+
+            if (!System.IO.File.Exists(textFilePath))
+            {
+                return NotFound("No se encontró el archivo de empleados");
+            }
+
+            var jsonData = await System.IO.File.ReadAllTextAsync(textFilePath);
+            var empleados = JsonSerializer.Deserialize<List<Empleado>>(jsonData);
+
+
+            return Ok(empleados);
+        }
+
+        [HttpGet]
+        [Route("SinDuplicados")]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetWithOutDuplicates()
+        {
+            var textFilePath = Path.Combine(Directory.GetCurrentDirectory(), "data.txt");
+
+            if (!System.IO.File.Exists(textFilePath))
+            {
+                return NotFound("No se encontró el archivo de empleados");
+            }
+
+            var jsonData = await System.IO.File.ReadAllTextAsync(textFilePath);
+            var empleados = JsonSerializer.Deserialize<List<Empleado>>(jsonData);
+
+            empleados = empleados.GroupBy(e => new { e.Name, e.LastName }).Select(g => g.First()).ToList();
+
+            return Ok(empleados);
+        }
+
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿namespace Prueba_tecnica_mardom_01.Models
+{
+    public class Empleado
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public int Salary { get; set; }
+        public char Gender { get; set; }
+        public string Position { get; set; }
+        public DateOnly StartDate { get; set; }
+
+    }
+}
